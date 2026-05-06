8860ca430bf6865eba36d62a514630676b5e4550
bwilk-umjaslo <bwilk-umjaslo@users.noreply.github.com>
2026-05-06 17:51:31 +0000

    deploy: 2368d907c19efa79c0308e4c103c94e39f57ddd5

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1eeff81e.js',
+                path_to_root + 'searchindex-a32265b9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
